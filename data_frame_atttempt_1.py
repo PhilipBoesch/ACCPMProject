10a5fdec2d6448236263a9b2cc7ee9afcecde602
PhilipBoesch <98331764+PhilipBoesch@users.noreply.github.com>
2022-02-08 17:29:03 +0000

    Add files via upload

--- data_frame_atttempt_1.py
+++ data_frame_atttempt_1.py
@@ -7,10 +7,9 @@ Created on Mon Jan 24 14:49:52 2022
 """
 
 import pandas as pd
-import numpy as np
 from crossref.restful import Works
 from datetime import datetime
-
+from datetime import date
 
 #read in MAIC excel sheet
 raw_csv = pd.read_excel("maic_corona_virus_data copy.xlsx")
@@ -19,54 +18,55 @@ raw_df = pd.DataFrame(raw_csv)
 #raw_csv = raw_df - both dataframes, I just made a copy here in case i mess up
 
 #Is study accepted into MAIC? -> dropping all rows with no maic input name
-# could have decided which were accepted numerouis ways - thought this would 
-# make the most sense since i need to run this col through maic anyway
 df_accepted = raw_df.dropna(subset=['Maic input name'])
-#also need to drop some more due to 'DU' or 'NMOI' etc entries - 
-#also typos dont help - noticed some quite late - 
-#in future: standardise either yes or no to save this type of hassle and error
-# managebale here, but if we have like 300000 studies it could be painful
+#also need to drop some more due to 'DU' or 'NMOI' etc entries
 df_accepted = df_accepted.drop(df_accepted[df_accepted["Maic input name"].isin(
     ["NMOI", "DU", "Not sure the data we need is available?",
      "Data Unavailable", "Data Unavaialble"])].index)
 
-#set length of new date column
-l = len(df_accepted['DOI'])
-#create new column named 'publication date' and set 'type' to datetime
-#need to look into getting rid of time  itself here here
-df_accepted['Publication date'] = pd.Series(np.random.randn(l), index=df_accepted.index)
-df_accepted['Publication date'] = pd.to_datetime(df_accepted['Publication date'], format='%d/%m/%Y')
-# create a fuction to automate date assignement, and add 
-
+# creating a cell below this line
+#%%
+# create a fuction to automate date assignement...
 #setting crossref variable to make code less painful
 works = Works()
-#retrieve doi metadata as dict - somehow doesnt come out as json?!
-# despite documents saying otherwise
-w1 = works.doi('10.1016/j.micinf.2004.11.004')
-#selecting the date of publishing from the above doi
-# in future could just add the puyblishing date to the main file in the first
-# place - tales up one column and is very useful for retrospective studies
-w2 = w1['published']
-w3 = w2['date-parts']
-#removing a set of square brackets to create str from list
-w4 = (", ".join( repr(e) for e in w3))
-#converting str into date
-datetime_object = datetime.strptime(w4, '[%Y, %m, %d]').date()
 
+#create new dict for pub date and doi
+pub_dict = {}
+# FOR loop for the above stuff to automate the process
 for i in df_accepted['DOI']:
-    d1 = works.doi(i)
+    d1 = works.doi(i) #retrieve doi metadata as dict
     try:
-        d2 = d1['published']
-    except TypeError:
-        print(i)
+        d2 = d1['published'] #select date of pub from doi metadata
+    except TypeError: # if problem in retrieval
+        pub_date = 'do this manually'
+        pub_dict[i] = pub_date
     else:
-        d3 = d2['date-parts']
-        d4 = (", ".join( repr(o) for o in d3))
+        d3 = d2['date-parts'] # extract str from dict
+        d4 = (", ".join( repr(o) for o in d3)) # remove a set of []
         try:
             pub_date = datetime.strptime(d4, '[%Y, %m, %d]').date()
-            print(pub_date)
+            pub_dict[i] = pub_date #set and add pub date to new dict
         except (ValueError, TypeError):
-            print(d4)
-
-
-
+            try:
+                pub_date = datetime.strptime(d4, '[%Y, %m]').date()
+                pub_dict[i] = pub_date
+            except ValueError: # in case no month is retrieved (ie year only)
+                pub_date = 'do this manually'
+                pub_dict[i] = pub_date
+# creating a cell below this line
+#%%
+# identifying the dois of dates that need to be added manually
+for key, ans in pub_dict.items():
+    if ans == 'do this manually':
+        print(key)
+# creating a cell below this line
+#%%
+# adding the dates manually
+pub_dict['10.1186/1471-2172-6-2'] = date.fromisoformat('2005-01-18')
+pub_dict['10.1086/503843,16652313'] = date.fromisoformat('2006-06-01')
+pub_dict['10.1016/j.immuni.2020.11.017.'] = date.fromisoformat('2020-12-15')
+# creating a cell below this line
+#%%
+# adding pub_dict to the main df as 'publication date'
+df_final = df_accepted
+df_final['Publication date']=df_final['DOI'].astype(str).map(pub_dict)
